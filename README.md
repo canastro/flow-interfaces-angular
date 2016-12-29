27009a7231a4b2c9864d4fd081492d2f3ac7c687
Ricardo Canastro <ricardo.canastro@blip.pt>
2016-12-29 09:39:24 +0000

    Add angular component and noop

--- README.md
+++ README.md
@@ -1,20 +1,15 @@
 # flow-interfaces-angular
-
 Flow interfaces allowing the use of Facebook's [Flow][flow] with [Angular JS][angularjs] projects.
-
-## Note
-
-Until https://github.com/facebook/flow/issues/199 is resolved, this repository is unfortunately less useful than one might hope.
+Forked from: https://github.com/solnet-aquarium/flow-interfaces-angular
 
 ## Usage
-
- - `bower install --save flow-interfaces-angular`
+ - `npm install --save flow-interfaces-angular`
  - `flow init` at the root of your project
  - Add a `.flowconfig` file with content that resembles:
 
  ```
  [libs]
- path/to/this/package/interfaces
+ node_modules/flow-interfaces-angular/interfaces
  ```
  - Run flow on your project
 
--- interfaces/angular.js
+++ interfaces/angular.js
@@ -1,4 +1,3 @@
-/* jshint ignore: start */
 type AngularJSIterable = Object | Array;
 type AngularJSStringNumber = string | number;
 type AngularJSDependencyDirectiveFunction = (...args: any) => Object;
@@ -6,40 +5,42 @@ type AngularJSDependencyControllerFunction = (...args: any) => void;
 type AngularJSDependencyFactoryFunction = (...args: any) => void;
 
 declare class AngularJSCallbacks {
-  css(index: number, value: string): AngularJSStringNumber;
-  iterator(value: any, key: string | number): void;
+    css(index: number, value: string): AngularJSStringNumber;
+    iterator(value: any, key: string | number): void;
 }
 
 declare class AngularJSJQueryLite {
-  bind(eventType: string, handler: Function): AngularJSJQueryLite;
-  css(properties: Object): AngularJSJQueryLite;
-  css(propertyName: string): string;
-  css(propertyName: string, propertyFunction: AngularJSCallbacks.css): AngularJSJQueryLite;
-  css(propertyName: string, value: string): AngularJSJQueryLite;
-  unbind(eventType: string): AngularJSJQueryLite;
-  unbind(eventType: string, handler: Function): AngularJSJQueryLite;
+    bind(eventType: string, handler: Function): AngularJSJQueryLite;
+    css(properties: Object): AngularJSJQueryLite;
+    css(propertyName: string): string;
+    css(propertyName: string, propertyFunction: AngularJSCallbacks.css): AngularJSJQueryLite;
+    css(propertyName: string, value: string): AngularJSJQueryLite;
+    unbind(eventType: string): AngularJSJQueryLite;
+    unbind(eventType: string, handler: Function): AngularJSJQueryLite;
 }
 
 declare class AngularJSModule {
-  controller(name: string, dependencies: Array<AngularJSDependencyControllerFunction | string>): AngularJSModule;
-  directive(name: string, directiveFactory: Array<AngularJSDependencyDirectiveFunction | string>): AngularJSModule;
-  directive(name: string, directiveFactory: Function): AngularJSModule;
-  directive(name: { name: Array<AngularJSDependencyDirectiveFunction | string> }): AngularJSModule;
-  factory(name: string, providerFunction: Array<AngularJSDependencyFactoryFunction | string>): AngularJSModule;
-  factory(name: string, providerFunction: AngularJSDependencyFactoryFunction): AngularJSModule;
-  value(name: string, object: any): AngularJSModule;
-  constant(name: string, object: any): AngularJSModule;
+    controller(name: string, dependencies: Array<AngularJSDependencyControllerFunction | string>): AngularJSModule;
+    component(name: string, componentFactory: Object): AngularJSModule;
+    directive(name: string, directiveFactory: Array<AngularJSDependencyDirectiveFunction | string>): AngularJSModule;
+    directive(name: string, directiveFactory: Function): AngularJSModule;
+    directive(name: { name: Array<AngularJSDependencyDirectiveFunction | string> }): AngularJSModule;
+    factory(name: string, providerFunction: Array<AngularJSDependencyFactoryFunction | string>): AngularJSModule;
+    factory(name: string, providerFunction: AngularJSDependencyFactoryFunction): AngularJSModule;
+    value(name: string, object: any): AngularJSModule;
+    constant(name: string, object: any): AngularJSModule;
 }
 
 declare class AngularJS {
-  // copy(object: Object | Array): Object | Array;
-  copy<T>(object: T): T;
-  element(element: string | Element): AngularJSJQueryLite;
-  extend<T>(dst: T, src: Object): T;
-  forEach(obj: AngularJSIterable, iterator: AngularJSCallbacks.iterator): AngularJSIterable;
-  forEach(obj: AngularJSIterable, iterator: AngularJSCallbacks.iterator, context: Object): AngularJSIterable;
-  module(name: string, dependencies: Array<string>): AngularJSModule;
-  module(name: string): AngularJSModule;
+    // copy(object: Object | Array): Object | Array;
+    copy<T>(object: T): T;
+    element(element: string | Element): AngularJSJQueryLite;
+    extend<T>(dst: T, src: Object): T;
+    forEach(obj: AngularJSIterable, iterator: AngularJSCallbacks.iterator): AngularJSIterable;
+    forEach(obj: AngularJSIterable, iterator: AngularJSCallbacks.iterator, context: Object): AngularJSIterable;
+    module(name: string, dependencies: Array<string>): AngularJSModule;
+    module(name: string): AngularJSModule;
+    noop: Function;
 }
 
 declare var angular: AngularJS;
